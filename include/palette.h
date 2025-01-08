9969f100353316341da96ae5056f6e058675934e
JammyStufff <88464669+JammyStufff@users.noreply.github.com>
2025-01-08 13:08:20 -0500

    dude im so awesome

--- include/palette.h
+++ include/palette.h
@@ -58,6 +58,8 @@ extern u16 ALIGNED(4) gPlttBufferUnfaded[PLTT_BUFFER_SIZE];
 extern u16 ALIGNED(4) gPlttBufferFaded[PLTT_BUFFER_SIZE];
 
 void LoadCompressedPalette(const u32 *src, u32 offset, u32 size);
+void HueShiftMonPalette(u16*, u32);
+void LoadHueShiftedMonPalette(const u32 *, u16, u16, u32);
 void LoadPalette(const void *src, u32 offset, u32 size);
 void FillPalette(u32 value, u32 offset, u32 size);
 void TransferPlttBuffer(void);
